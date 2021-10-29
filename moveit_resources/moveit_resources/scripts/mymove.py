61bb70c2e0fb0e85aa9798f44502e92c57568755
Sandeepan Ghosh <88130555+san2130@users.noreply.github.com>
2021-10-29 16:30:58 +0530

    Add files via upload

--- moveit_resources/moveit_resources/scripts/mymove.py
+++ moveit_resources/moveit_resources/scripts/mymove.py
@@ -5,7 +5,9 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
+from geometry_msgs.msg import Twist
 
+  
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('move_group_tut',anonymous=True)
 
@@ -13,23 +15,27 @@ robot=moveit_commander.RobotCommander()
 scene=moveit_commander.PlanningSceneInterface()
 group=moveit_commander.MoveGroupCommander("robotic_arm")
 display_trajectory_publisher=rospy.Publisher('/arm/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory,queue_size=10)
-
-pose_target=geometry_msgs.msg.Pose()
-pose_target.orientation.w=1.0
-pose_target.position.x=0.009
-pose_target.position.y=0.0
-pose_target.position.z=0.1
+joints=rospy.wait_for_message("/arm/inverse_kin",Twist)
+#pose_target=geometry_msgs.msg.Pose()
+#pose_target.orientation.w=1.0
+#pose_target.position.x=0.009
+#pose_target.position.y=0.0
+#pose_target.position.z=0.1
 #group.set_pose_target(pose_target)
-#group.set_planning_pipeline_id("ompl")
+group.set_planning_pipeline_id("chomp")
 #group.set_planner_id("RRT")
-#group.set_planning_time(5)
+group.set_planning_time(5)
 #print(group.get_current_joint_values())
 
-#group.set_num_planning_attempts(10)
+group.set_num_planning_attempts(10)
 #rospy.loginfo(group.get_planning_pipeline_id())
 #plan1=group.plan()
 #print(plan1[0])
-group.go([20.0,0.0,58.0,92.0])
+print(joints)
+group.go([0.0,23.0,0.0,0.0,0.0])
+#group.go([joints.linear.x,joints.linear.y,joints.linear.z,joints.angular.x,2.0])
+#rospy.sleep(2)
+#group.go([0.0,0.0,0.0,0.0,0.0])
 rospy.sleep(5)
 
 moveit_commander.roscpp_shutdown()
